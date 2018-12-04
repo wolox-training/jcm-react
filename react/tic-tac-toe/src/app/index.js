bd429c120627356e61f3f257afcf868fc823ea76
Pablo Ferro <pablo.pyp@gmail.com>
2018-12-04 11:53:56 -0300

    Tic Tac Toe tutorial base

---
+++ react/tic-tac-toe/src/app/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Game from './screens/Game';
+
+import '../scss/application.scss';
+
+const App = () => <Game />;
+
+export default App;
---
+++ react/tic-tac-toe/src/app/screens/Game/components/Square/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './styles.module.scss';
+
+const Square = () => (
+  <button type="button" className={styles.square}>
+    {/* TODO */}
+  </button>
+);
+
+export default Square;
