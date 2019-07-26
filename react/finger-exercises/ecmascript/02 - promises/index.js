2b246a79598dae9b86be8fb96fd9f5bc89a740df
Juan Camilo Morales <camov1209@gmail.com>
2019-07-26 17:23:14 -0500

    complete delay function

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,7 +1,13 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
+export function delay(time) {
+  const startDate = Date.now();
 
+  return new Promise((resolve) => {
+    setInterval(() => {
+      resolve(Date.now() - startDate);
+    }, time);
+  });
 }
 
 export function asyncDelay() {
--- react/finger-exercises/ecmascript/02 - promises/test.js
+++ react/finger-exercises/ecmascript/02 - promises/test.js
@@ -3,12 +3,11 @@ import { delay, asyncDelay } from '.';
 const TIME = 500;
 
 describe('Promises', () => {
-  xit('delay works well', () => delay(TIME)
+  it('delay works well', () => delay(TIME)
     .then(delayedTime => {
       expect(delayedTime).toBeGreaterThanOrEqual(TIME - 100);
       expect(delayedTime).toBeLessThanOrEqual(TIME + 100);
     }));
-
   xit('delay will not wait if the time is too long', () => delay(TIME * TIME)
     .catch(e => expect(e).toEqual(Error('This time is too much !'))));
 
