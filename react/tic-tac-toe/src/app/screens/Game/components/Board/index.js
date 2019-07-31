2dbfe19fea518a9762c226b418d71626dbfc290d
Juan Camilo Morales <camov1209@gmail.com>
2019-07-31 15:55:03 -0500

    add logging move

--- react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
+++ react/tic-tac-toe/src/app/screens/Game/components/Board/index.js
@@ -1,57 +1,24 @@
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 
 import Square from '../Square';
 
-import calculateWinner from './utils';
 import styles from './styles.module.scss';
 
 class Board extends Component {
-  state = {
-    squares: Array(9).fill(null),
-    xIsNext: true
-  }
-
-  nextTurn() {
-    return this.state.xIsNext ? 'X' : 'O';
-  }
-
-  handleClick(i) {
-    const { squares } = this.state;
-    
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-
-    squares[i] = this.nextTurn();
-    this.setState(prevState => ({
-      squares,
-      xIsNext: !prevState.xIsNext
-    }));
-  }
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        // eslint-disable-next-line react/jsx-no-bind
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status = '';
-
-    if (winner) {
-      status = `Winner: ${winner}`;
-    } else {
-      status = `Next player: ${this.nextTurn()}`;
-    }
-
     return (
       <div>
-        <div className={styles.status}>{status}</div>
         <div className={styles.boardRow}>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- react/tic-tac-toe/src/app/screens/Game/index.js
+++ react/tic-tac-toe/src/app/screens/Game/index.js
@@ -1,20 +1,67 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-bind */
+import React, { Component } from 'react';
 
+import calculateWinner from './utils';
 import styles from './styles.module.scss';
 import Board from './components/Board';
 
-function Game () {
-  return (
-    <div className={styles.game}>
-      <div className={styles.gameBoard}>
-        <Board />
+class Game extends Component {
+  state = {
+    history: [{
+      squares: Array(9).fill(null)
+    }],
+    xIsNext: true
+  };
+
+  nextTurn() {
+    return this.state.xIsNext ? 'X' : 'O';
+  }
+
+  handleClick(i) {
+    const { history } = this.state;
+    const current = history[history.length - 1];
+    const squares = current.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    squares[i] = this.nextTurn();
+    this.setState(prevState => ({
+      history: history.concat([{
+        squares
+      }]),
+      xIsNext: !prevState.xIsNext
+    }));
+  }
+
+  render() {
+    const { history } = this.state;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status = '';
+
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.nextTurn()}`;
+    }
+
+    return (
+      <div className={styles.game}>
+        <div className={styles.gameBoard}>
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
+        </div>
+        <div className={styles.gameInfo}>
+          <div>{ status }</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
-      <div className={styles.gameInfo}>
-        <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Game;
